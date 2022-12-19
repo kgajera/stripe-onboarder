5e1731d379872ca5043d63f314c6c831d68284d7
Mathias Lorenzen <mathias.lorenzen@live.com>
2022-12-19 13:01:15 +0100

    Fix for broken tests.

--- src/tasks/puppeteer.ts
+++ src/tasks/puppeteer.ts
@@ -1,9 +1,5 @@
 import type { Page } from "puppeteer";
 
 export async function waitForNavigation(page: Page) {
-  await page.waitForNetworkIdle({ idleTime: 500 });
-  await page.waitForFunction(() => document.readyState === "complete");
-  await page.waitForNetworkIdle({ idleTime: 500 });
-  await page.waitForFunction(() => document.readyState === "complete");
-  await page.waitForNetworkIdle({ idleTime: 500 });
+  await page.waitForNetworkIdle({ idleTime: 3000 });
 }
